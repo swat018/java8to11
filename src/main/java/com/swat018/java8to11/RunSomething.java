11c881a982cb91b58782ec43e708d4f6fa11c763
jinwoo park <swat018@gmail.com>
2020-07-05 16:08:05 +0900

    Java 8 - 함수형 인터페이스와 람다 표현식

---
+++ src/main/java/com/swat018/java8to11/RunSomething.java
@@ -0,0 +1,15 @@
+package com.swat018.java8to11;
+
+@FunctionalInterface
+public interface RunSomething {
+
+    int doIt(int number);
+
+/*    static void printName() {
+        System.out.println("jinwoo");
+    }
+
+    default void printAge() {
+        System.out.println("37");
+    }*/
+}
