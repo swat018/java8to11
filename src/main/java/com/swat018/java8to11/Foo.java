779af385f5b0e6c0b3bda2fff8492fd04c7eb6b1
jinwoo park <swat018@gmail.com>
2020-08-11 00:00:06 +0900

    Java 8 - 함수형 인터페이스와 람다 표현식

--- src/main/java/com/swat018/java8to11/Foo.java
+++ src/main/java/com/swat018/java8to11/Foo.java
@@ -3,7 +3,9 @@ package com.swat018.java8to11;
 public class Foo {
 
     public static void main(String[] args) {
+
         int baseNumber = 10;
         RunSomething runSomething = number -> number + baseNumber;
+
     }
 }
--- src/main/java/com/swat018/java8to11/RunSomething.java
+++ src/main/java/com/swat018/java8to11/RunSomething.java
@@ -5,11 +5,4 @@ public interface RunSomething {
 
     int doIt(int number);
 
-/*    static void printName() {
-        System.out.println("jinwoo");
-    }
-
-    default void printAge() {
-        System.out.println("37");
-    }*/
 }
