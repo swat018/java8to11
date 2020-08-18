b54d4ce5d7d8cfcef5da842a0258fd89a835dbeb
jinwoo park <swat018@gmail.com>
2020-08-18 23:28:59 +0900

    Java 8 - Stream API example before

--- src/main/java/com/swat018/java8to11/App.java
+++ src/main/java/com/swat018/java8to11/App.java
@@ -68,11 +68,11 @@ public class App {
         Foo.printAnything();*/
 
         // 7. 자바 8 API의 기본 메소드와 스태틱 메소드
-        List<String> names = new ArrayList<>();
+ /*       List<String> names = new ArrayList<>();
         names.add("keesun");
         names.add("whiteship");
         names.add("tody");
-        names.add("foo");
+        names.add("foo");*/
 
 //        name.forEach(System.out::println);
 
@@ -104,11 +104,52 @@ public class App {
         System.out.println("==================");
         names.forEach(System.out::println);*/
 
-        List<String> collect = names.parallelStream().map((s) -> {
+/*        List<String> collect = names.parallelStream().map((s) -> {
             System.out.println(s + " " + Thread.currentThread().getName());
             return s.toUpperCase();
         }).collect(Collectors.toList());
-        collect.forEach(System.out::println);
+        collect.forEach(System.out::println);*/
+
+        // 9. Stream API
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> keesunEvents = new ArrayList<>();
+        keesunEvents.add(springClasses);
+        keesunEvents.add(javaClasses);
+
+        System.out.println("spring 으로 시작하는 수업");
+        // TODO
+
+        System.out.println("close 되지 않은 수업");
+        // TODO
+
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        // TODO
+
+
+
+        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        // TODO
+
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        // TODO
+
+        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        // TODO
+
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
+        // TODO
+        
     }
 
 /*    private void run() {
