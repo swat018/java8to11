a6f2a60e120db0fd50389aca6b2478dfd049259b
jinwoo park <swat018@gmail.com>
2020-08-19 23:47:50 +0900

    Java 8 - Stream API

--- src/main/java/com/swat018/java8to11/App.java
+++ src/main/java/com/swat018/java8to11/App.java
@@ -128,27 +128,42 @@ public class App {
         keesunEvents.add(javaClasses);
 
         System.out.println("spring 으로 시작하는 수업");
-        // TODO
+        springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .forEach(oc-> System.out.println(oc.getId()));
 
         System.out.println("close 되지 않은 수업");
-        // TODO
+        springClasses.stream()
+//                .filter(oc -> !oc.isClosed())
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(oc -> System.out.println(oc.getId()));
 
         System.out.println("수업 이름만 모아서 스트림 만들기");
-        // TODO
-
-
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
 
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
-        // TODO
+        keesunEvents.stream()
+                .flatMap(Collection::stream)
+                .forEach(oc -> System.out.println(oc.getId()));
 
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
-        // TODO
+        Stream.iterate(10, i -> i+1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
 
         System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
-        // TODO
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println(test);
 
-        System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
-        // TODO
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        List<String> spring = springClasses.stream()
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(OnlineClass::getTitle)
+                .collect(Collectors.toList());
+        spring.forEach(System.out::println);
         
     }
 
