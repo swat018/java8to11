10f8d30c29a587838032d79b883d670e6646f6c7
jinwoo park <swat018@gmail.com>
2021-08-08 15:59:17 +0900

    3. 자바에서 제공하는 함수형 인터페이스

--- src/main/java/swat018/java8to11/Foo.java
+++ src/main/java/swat018/java8to11/Foo.java
@@ -1,28 +1,33 @@
 package swat018.java8to11;
 
+import java.util.function.*;
+
 public class Foo {
 
     public static void main(String[] args) {
-//        // 익명 내부 클래스 anonymous inner class
-//        RunSomething runSomething = () -> System.out.println("Hello");
-//        runSomething.doIt();
+//        Plus10 plus10 = new Plus10();
+//        System.out.println(plus10.apply(1));
 
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        System.out.println(plus10.apply(1));
+        System.out.println(multiply2.apply(1));
 
-//        RunSomething runSomething = (number) -> {
-//            return number + 10;
-//        };
-//
-//        System.out.println(runSomething.doIt(1));
-//        System.out.println(runSomething.doIt(1));
-//        System.out.println(runSomething.doIt(1));
-//
-//        System.out.println(runSomething.doIt(2));
-//        System.out.println(runSomething.doIt(2));
-//        System.out.println(runSomething.doIt(2));
-//    }
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
+        System.out.println(plus10.andThen(multiply2).apply(2));
 
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
 
-        int baseNumber = 10;
-        RunSomething runSomething = number -> number + baseNumber;
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
+
+        Predicate<String> startsWithJinwoo = (s) -> s.startsWith("jinwoo");
+        Predicate<Integer> isEven = (i) -> 1%2 ==0;
+
+        UnaryOperator<Integer> plus12 = (i) -> i + 12;
+        UnaryOperator<Integer> multiply4 = (i) -> i * 4;
+        System.out.println(plus12.andThen(multiply4).apply(2));
     }
 }
