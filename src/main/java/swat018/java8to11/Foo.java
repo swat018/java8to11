e48a801028afecc2d5c502899de71e250b1320b0
jinwoo park <swat018@gmail.com>
2021-12-18 23:33:31 +0900

    4. 람다 표현식

--- src/main/java/swat018/java8to11/Foo.java
+++ src/main/java/swat018/java8to11/Foo.java
@@ -8,26 +8,63 @@ public class Foo {
 //        Plus10 plus10 = new Plus10();
 //        System.out.println(plus10.apply(1));
 
-        Function<Integer, Integer> plus10 = (i) -> i + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i * 2;
-        System.out.println(plus10.apply(1));
-        System.out.println(multiply2.apply(1));
+//        Function<Integer, Integer> plus10 = (i) -> i + 10;
+//        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+//        System.out.println(plus10.apply(1));
+//        System.out.println(multiply2.apply(1));
+//
+//        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+//        System.out.println(multiply2AndPlus10.apply(2));
+//        System.out.println(plus10.andThen(multiply2).apply(2));
+//
+//        Consumer<Integer> printT = (i) -> System.out.println(i);
+//        printT.accept(10);
+//
+//        Supplier<Integer> get10 = () -> 10;
+//        System.out.println(get10.get());
+//
+//        Predicate<String> startsWithJinwoo = (s) -> s.startsWith("jinwoo");
+//        Predicate<Integer> isEven = (i) -> 1%2 ==0;
 
-        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
-        System.out.println(multiply2AndPlus10.apply(2));
-        System.out.println(plus10.andThen(multiply2).apply(2));
+//        UnaryOperator<Integer> plus12 = (i) -> i + 12;
+//        UnaryOperator<Integer> multiply4 = (i) -> i * 4;
+//        System.out.println(plus12.andThen(multiply4).apply(2));
 
-        Consumer<Integer> printT = (i) -> System.out.println(i);
-        printT.accept(10);
+        Supplier<Integer> get10 =  () -> 10;
 
-        Supplier<Integer> get10 = () -> 10;
-        System.out.println(get10.get());
+        BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
+        System.out.println(sum.apply(1,2));
 
-        Predicate<String> startsWithJinwoo = (s) -> s.startsWith("jinwoo");
-        Predicate<Integer> isEven = (i) -> 1%2 ==0;
+        Foo foo = new Foo();
+        foo.run();
 
-        UnaryOperator<Integer> plus12 = (i) -> i + 12;
-        UnaryOperator<Integer> multiply4 = (i) -> i * 4;
-        System.out.println(plus12.andThen(multiply4).apply(2));
+    }
+
+    private void run() {
+        int baseNumber = 10;
+
+        // 로컬 클래스
+        class localClass {
+            void printBaseNumber() {
+                int baseNumber = 11;
+                System.out.println(baseNumber); //   11
+            }
+        }
+
+
+        // 익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer baseNumber) {
+                System.out.println(baseNumber);
+            }
+        };
+
+        // 람다
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+
+        printInt.accept(10);
     }
 }
