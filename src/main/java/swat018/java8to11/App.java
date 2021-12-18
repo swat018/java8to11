ad4bc7ad5708ffd26d0fed1cbdd7fbbd7d594f7a
jinwoo park <swat018@gmail.com>
2021-12-19 00:23:11 +0900

    5. 메소드 레퍼런스

---
+++ src/main/java/swat018/java8to11/App.java
@@ -0,0 +1,35 @@
+package swat018.java8to11;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class App {
+
+    public static void main(String[] args) {
+//        UnaryOperator<String> hi = (s) -> "hi " + s;
+
+        Greeting greeting = new Greeting();
+
+        UnaryOperator<String> hi = Greeting::hi;
+        UnaryOperator<String> hello= greeting::hello;
+        System.out.println(hello.apply("jinwoo"));
+
+        Supplier<Greeting> newGreeting = Greeting::new;
+        newGreeting.get();
+
+        Function<String, Greeting> jinwooGreeting = Greeting::new;
+        Greeting jinwoo = jinwooGreeting.apply("jinwoo");
+        System.out.println(jinwoo.getName());
+
+
+        String[] names = {"jinwoo", "swat018", "keesun"};
+
+        Arrays.sort(names, (o1, o2) -> 0);
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
+
+    }
+}
---
+++ src/main/java/swat018/java8to11/Greeting.java
@@ -0,0 +1,25 @@
+package swat018.java8to11;
+
+public class Greeting {
+
+    private String name;
+
+    public Greeting() {
+        this.name = name;
+    }
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+    public static String hi(String name) {
+        return "hi " + name;
+    }
+}
