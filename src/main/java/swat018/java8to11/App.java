3e0827de8c058516cdd94f24ee6d1fe6d14237a2
jinwoo park <swat018@gmail.com>
2021-12-24 01:10:28 +0900

    9. Stream API

--- src/main/java/swat018/java8to11/App.java
+++ src/main/java/swat018/java8to11/App.java
@@ -2,6 +2,7 @@ package swat018.java8to11;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collector;
@@ -79,31 +80,81 @@ public class App {
 //        name.sort(compareToIgnoreCase.reversed());
 //        name.forEach(System.out::println);
 
-        List<String> names = new ArrayList<>();
-        names.add("jinwoo");
-        names.add("seojin");
-        names.add("hyebin");
-        names.add("taeyang");
-        names.add("hyunji");
+//        List<String> names = new ArrayList<>();
+//        names.add("jinwoo");
+//        names.add("seojin");
+//        names.add("hyebin");
+//        names.add("taeyang");
+//        names.add("hyunji");
+//
+//        Stream<String> stringStream = names.stream().map(String::toUpperCase);
+//
+//        List<String> collect = names.stream().map((s) -> {
+//            System.out.println(s);
+//            return s.toUpperCase();
+//        }).collect(Collectors.toList());
+//        collect.forEach(System.out::println);
+//
+//        System.out.println("==============");
+//
+//        names.forEach(System.out::println);
+//
+//        List<String> collect1 = names.parallelStream().map((s) -> {
+//            System.out.println(s + " " + Thread.currentThread().getName());
+//            return s.toUpperCase();
+//        }).collect(Collectors.toList());
+//        collect1.forEach(System.out::println);
 
-        Stream<String> stringStream = names.stream().map(String::toUpperCase);
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        List<String> collect = names.stream().map((s) -> {
-            System.out.println(s);
-            return s.toUpperCase();
-        }).collect(Collectors.toList());
-        collect.forEach(System.out::println);
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream()
+                .filter( oc -> oc.getTitle().startsWith("spring"))
+                .forEach(oc -> System.out.println(oc.getId()));
 
-        System.out.println("==============");
+        System.out.println("close 되지 않은 수업");
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(oc -> System.out.println(oc.getId()));
 
-        names.forEach(System.out::println);
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
 
-        List<String> collect1 = names.parallelStream().map((s) -> {
-            System.out.println(s + " " + Thread.currentThread().getName());
-            return s.toUpperCase();
-        }).collect(Collectors.toList());
-        collect1.forEach(System.out::println);
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
 
+        List<List<OnlineClass>> keesunEvents = new ArrayList<>();
+        keesunEvents.add(springClasses);
+        keesunEvents.add(javaClasses);
 
+        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        keesunEvents.stream().flatMap(Collection::stream)
+                .forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10, i -> i + 1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        List<String> spring = springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .filter(t -> t.contains("spring"))
+                .collect(Collectors.toList());
+        spring.forEach(System.out::println);
     }
 }
